746befa7d248f16edd710fc8f0a07c893687503c
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-07-30 19:36:51 +0300

    Fix lang error

--- src/App.js
+++ src/App.js
@@ -17,35 +17,14 @@ class App extends Component {
     };
   }
 
-  applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+  applyPickedLanguage = (pickedLanguage) => {
     document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
-  };
-
-  swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
+    this.loadResumeFromPath(`res_primaryLanguage.json`);
   };
 
   componentDidMount = () => {
     this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
+    this.applyPickedLanguage(window.$primaryLanguage);
   };
 
   loadResumeFromPath = (path) => {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,8 +20,6 @@ class Footer extends Component {
           <div className="row">
             <div className="col social-links">{networks}</div>
             <div className="col">
-            </div>
-            <div className="col">
               <div className="copyright py-4 text-center">
                 <div className="container">
                   <small>
